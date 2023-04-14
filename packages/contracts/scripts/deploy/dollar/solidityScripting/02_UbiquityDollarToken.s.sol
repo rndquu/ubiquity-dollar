136e4535fe7a5a7ad806c2896647a6e40de72d60
rndquu <rndquu@gmail.com>
2023-04-14 11:33:27 +0300

    feat: coverage increased

--- packages/contracts/scripts/deploy/dollar/solidityScripting/02_UbiquityDollarToken.s.sol
+++ packages/contracts/scripts/deploy/dollar/solidityScripting/02_UbiquityDollarToken.s.sol
@@ -1,41 +1,41 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "./01_Diamond.s.sol";
+// // SPDX-License-Identifier: MIT
+// pragma solidity 0.8.19;
+// import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// import "./01_Diamond.s.sol";
 
-contract DollarScript is DiamondScript {
-    UbiquityDollarToken dollar;
-    address metapool;
+// contract DollarScript is DiamondScript {
+//     UbiquityDollarToken dollar;
+//     address metapool;
 
-    function run() public virtual override {
-        super.run();
-        vm.startBroadcast(deployerPrivateKey);
+//     function run() public virtual override {
+//         super.run();
+//         vm.startBroadcast(deployerPrivateKey);
 
-        dollar = new UbiquityDollarToken(address(diamond));
-        IManager.setDollarTokenAddress(address(dollar));
-        IAccessCtrl.grantRole(DOLLAR_TOKEN_MINTER_ROLE, address(diamond));
-        IAccessCtrl.grantRole(DOLLAR_TOKEN_BURNER_ROLE, address(diamond));
+//         dollar = new UbiquityDollarToken(address(diamond));
+//         IManager.setDollarTokenAddress(address(dollar));
+//         IAccessCtrl.grantRole(DOLLAR_TOKEN_MINTER_ROLE, address(diamond));
+//         IAccessCtrl.grantRole(DOLLAR_TOKEN_BURNER_ROLE, address(diamond));
 
-        dollar.mint(address(diamond), 10000e18);
-        uint256 adminBal = IERC20(curve3PoolToken).balanceOf(admin);
-        console.log("----ADMIN 3CRV bal:", adminBal);
-        // deployer needs 10000  3CRV to deploy the pool
-        IERC20(curve3PoolToken).transfer(address(diamond), 10000e18);
-        uint256 diamondBal = IERC20(curve3PoolToken).balanceOf(
-            address(diamond)
-        );
-        console.log("----DIAMOND 3CRV bal:", diamondBal);
-        //  deal(curve3PoolToken, address(diamond), 10000e18);
-        IManager.deployStableSwapPool(
-            curveFactory,
-            basePool,
-            curve3PoolToken,
-            10,
-            5000000
-        );
+//         dollar.mint(address(diamond), 10000e18);
+//         uint256 adminBal = IERC20(curve3PoolToken).balanceOf(admin);
+//         console.log("----ADMIN 3CRV bal:", adminBal);
+//         // deployer needs 10000  3CRV to deploy the pool
+//         IERC20(curve3PoolToken).transfer(address(diamond), 10000e18);
+//         uint256 diamondBal = IERC20(curve3PoolToken).balanceOf(
+//             address(diamond)
+//         );
+//         console.log("----DIAMOND 3CRV bal:", diamondBal);
+//         //  deal(curve3PoolToken, address(diamond), 10000e18);
+//         IManager.deployStableSwapPool(
+//             curveFactory,
+//             basePool,
+//             curve3PoolToken,
+//             10,
+//             5000000
+//         );
 
-        metapool = IManager.stableSwapMetaPoolAddress();
+//         metapool = IManager.stableSwapMetaPoolAddress();
 
-        vm.stopBroadcast();
-    }
-}
+//         vm.stopBroadcast();
+//     }
+// }
--- packages/contracts/scripts/deploy/dollar/solidityScripting/03_UbiquityGovernanceToken.s.sol
+++ packages/contracts/scripts/deploy/dollar/solidityScripting/03_UbiquityGovernanceToken.s.sol
@@ -1,22 +1,22 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+// // SPDX-License-Identifier: MIT
+// pragma solidity 0.8.19;
 
-import "./02_UbiquityDollarToken.s.sol";
+// import "./02_UbiquityDollarToken.s.sol";
 
-contract GovernanceScript is DollarScript {
-    UbiquityGovernanceToken governance;
+// contract GovernanceScript is DollarScript {
+//     UbiquityGovernanceToken governance;
 
-    function run() public virtual override {
-        super.run();
-        vm.startBroadcast(deployerPrivateKey);
+//     function run() public virtual override {
+//         super.run();
+//         vm.startBroadcast(deployerPrivateKey);
 
-        governance = new UbiquityGovernanceToken(address(diamond));
+//         governance = new UbiquityGovernanceToken(address(diamond));
 
-        IManager.setGovernanceTokenAddress(address(governance));
-        // grant diamond token admin rights
-        IAccessCtrl.grantRole(GOVERNANCE_TOKEN_MINTER_ROLE, address(diamond));
-        IAccessCtrl.grantRole(GOVERNANCE_TOKEN_BURNER_ROLE, address(diamond));
+//         IManager.setGovernanceTokenAddress(address(governance));
+//         // grant diamond token admin rights
+//         IAccessCtrl.grantRole(GOVERNANCE_TOKEN_MINTER_ROLE, address(diamond));
+//         IAccessCtrl.grantRole(GOVERNANCE_TOKEN_BURNER_ROLE, address(diamond));
 
-        vm.stopBroadcast();
-    }
-}
+//         vm.stopBroadcast();
+//     }
+// }
